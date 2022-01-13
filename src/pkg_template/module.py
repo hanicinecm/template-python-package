1fd27405ea6e428c3d081dcbc3cb3bc00e246ece
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-13 09:24:26 +0100

    fix formatting

--- src/pkg_template/module.py
+++ src/pkg_template/module.py
@@ -28,7 +28,7 @@ def get_columns(n: int) -> list[str]:
     Returns:
         list of str letters starting with "a" and of length n.
     """
-    return list('abcdefghijklmnopqrstuvwxyz'[:n])
+    return list("abcdefghijklmnopqrstuvwxyz"[:n])
 
 
 def get_dataframe(shape: tuple[int, int]) -> pd.DataFrame:
